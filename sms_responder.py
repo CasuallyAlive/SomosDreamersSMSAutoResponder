024c2d08b4c55bcc39f840bf19b4adbd01f9c1ed
Jordy Alejandro Larrea Rodriguez <Jordy.larrearodriguez@gmail.com>
2023-04-23 04:02:24 -0600

    Changed .txt path

--- sms_responder.py
+++ sms_responder.py
@@ -5,7 +5,7 @@ import pandas as pd
 from twilio.rest import Client
 from twilio.twiml.messaging_response import MessagingResponse
 
-OPTIONS_PATH = "keywords_responses.txt"
+OPTIONS_PATH = "Resources/keywords_responses.txt"
 
 options_dict = {}
 # Load keywords and responses
